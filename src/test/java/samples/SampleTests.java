fbf259b094f1fc428023d45b3d0d4def51c4c9fc
rcsarro <rob@robsarro.com>
2019-11-07 20:17:15 -0500

    fixed formatting

--- src/test/java/samples/SampleTests.java
+++ src/test/java/samples/SampleTests.java
@@ -3,7 +3,6 @@ package samples;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -11,23 +10,19 @@ import java.util.Properties;
 import static io.restassured.RestAssured.*;
 
 public class SampleTests {
-    Properties prop = new Properties();
+	Properties prop = new Properties();
 
-    @BeforeTest
-    public void getPropertiesFile() throws IOException {
-    	// ToDo: Figure out relative path, this was not working 
-        FileInputStream fis = new FileInputStream("src/main/java/resources/config.properties");
-        prop.load(fis);
-    }
+	@BeforeTest
+	public void getPropertiesFile() throws IOException {
+		// ToDo: Figure out relative path, this was not working
+		FileInputStream fis = new FileInputStream("src/main/java/resources/config.properties");
+		prop.load(fis);
+	}
 
-
-    @Test
-    public void castlemockTest() {
-        baseURI = prop.getProperty("baseURI");
-        given().
-                get("/mock/rest/project/mhAjF9/application/izoQVd/users").
-                then().
-                assertThat().statusCode(200);
-    }
+	@Test
+	public void castlemockTest() {
+		baseURI = prop.getProperty("baseURI");
+		given().get("/mock/rest/project/mhAjF9/application/izoQVd/users").then().assertThat().statusCode(200);
+	}
 
 }
